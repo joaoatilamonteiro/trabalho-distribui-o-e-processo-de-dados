b281700a66cd2202325127a58941bfefffd46355
Luiz Matheus Silva Carvalho <113390965+luizmatheusdp@users.noreply.github.com>
2026-05-29 17:49:07 -0300

    Update sensor_estaciona.py

--- Estacionamento_supremo/sensores/sensor_estaciona.py
+++ Estacionamento_supremo/sensores/sensor_estaciona.py
@@ -25,15 +25,25 @@ sensor_id = "P1"
 # =========================
 
 def enviar_dados():
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     while True:
 
         msg = messages_pb2.SensorData()
+
         msg.sensor_id = sensor_id
         msg.sensor_type = "parking"
-        msg.value = random.randint(0, 100)  # vagas livres
-        msg.unit = "spots"
+
+        # vaga aleatória
+        msg.vaga_id = random.randint(1, 100)
+
+        # estado da vaga
+        msg.acao = random.choice([
+            "OCUPADA",
+            "LIVRE"
+        ])
+
         msg.timestamp = int(time.time())
 
         sock.sendto(
@@ -41,7 +51,9 @@ def enviar_dados():
             (GATEWAY_IP, GATEWAY_PORT)
         )
 
-        print(f"[{sensor_id}] vagas: {msg.value}")
+        print(
+            f"[{sensor_id}] vaga {msg.vaga_id} -> {msg.acao}"
+        )
 
         time.sleep(3)
 
@@ -75,16 +87,20 @@ def escutar_multicast():
 
         data, addr = sock.recvfrom(1024)
 
-        if data.decode() == "DISCOVER_SENSORS":
+        if data.decode(errors="ignore") == "DISCOVER_SENSORS":
 
             msg = messages_pb2.DiscoveryMessage()
+
             msg.sensor_id = sensor_id
             msg.sensor_type = "parking"
             msg.ip = "127.0.0.1"
             msg.tcp_port = 7000
             msg.active = True
 
-            sock.sendto(msg.SerializeToString(), addr)
+            sock.sendto(
+                msg.SerializeToString(),
+                addr
+            )
 
             print(f"[{sensor_id}] respondeu descoberta")
 
@@ -97,8 +113,15 @@ if __name__ == "__main__":
 
     print("Sensor de estacionamento iniciado...")
 
-    threading.Thread(target=escutar_multicast, daemon=True).start()
-    threading.Thread(target=enviar_dados, daemon=True).start()
+    threading.Thread(
+        target=escutar_multicast,
+        daemon=True
+    ).start()
+
+    threading.Thread(
+        target=enviar_dados,
+        daemon=True
+    ).start()
 
     while True:
         time.sleep(1)
